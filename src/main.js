7897fad82192cb678f1b11cc1a9521200d4ffe36
Alex J <alexjovermorales@gmail.com>
2018-01-06 11:47:22 +0100

    Register service worker

--- src/main.js
+++ src/main.js
@@ -28,3 +28,12 @@ new Vue({
   router,
   render: h => h(App)
 })
+
+const prod = process.env.NODE_ENV === 'production'
+const shouldSW = 'serviceWorker' in navigator && prod
+
+if (shouldSW) {
+  navigator.serviceWorker.register('/service-worker.js').then(function() {
+    console.log('Service Worker Registered');
+  })
+}
