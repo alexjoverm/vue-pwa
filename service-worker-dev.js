0e24a098c61552e9ab9900b33444f8b9e358f5e9
Alex J <alexjovermorales@gmail.com>
2018-01-13 14:17:26 +0100

    Add dev service worker

---
+++ service-worker-dev.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', () => self.skipWaiting());
+
+self.addEventListener('activate', () => {
+  self.clients.matchAll({ type: 'window' }).then(windowClients => {
+    for (let windowClient of windowClients) {
+      windowClient.navigate(windowClient.url);
+    }
+  });
+});
